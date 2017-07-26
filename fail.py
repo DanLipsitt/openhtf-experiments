95760db0e9ce86fb16e9c1889b9da7bfcf5b5260
Dan Lipsitt <danlipsitt@gmail.com>
2017-07-26 14:13:39 -0700

    Fixed bug in fail.py.

--- fail.py
+++ fail.py
@@ -4,16 +4,17 @@ from openhtf.output.callbacks import json_factory
 
 
 def phase1(test):
-    pass
+    print('phase 1 (pass)')
 
 
 @htf.measures(htf.Measurement('x').in_range(0, 1))
 def phase2(test):
     test.measurements.x = -1
+    print('phase 2 (fail)')
 
 
 def phase3(test):
-    test.logger.warning("We don't want to make it here")
+    print("phase 3 (we shouldn't get here!)")
 
 
 if __name__ == '__main__':
@@ -22,9 +23,10 @@ if __name__ == '__main__':
     test = htf.Test(
         phase1,
         phase2,
-        test_name='phase naming experiment'
+        phase3,
+        test_name='phase failure demo'
     )
 
     test.add_output_callbacks(json_factory.OutputToJSON(sys.stdout, indent=4))
 
-    test.execute(test_start=lambda: "example failed phase")
+    test.execute(test_start=lambda: "dut")
