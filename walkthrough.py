0ff8ac5a8ed96d52da843dd022b4607b0ec9df19
Dan Lipsitt <danlipsitt@gmail.com>
2017-08-01 19:39:32 -0700

    Walkthrough of desired behavior.

--- walkthrough.py
+++ walkthrough.py
@@ -4,7 +4,24 @@ from __future__ import print_function
 import openhtf as htf
 from openhtf.plugs import user_input
 from openhtf.output.callbacks import json_factory, console_summary
-from util import is_fail
+from util import is_fail, get_measurement
+
+
+@htf.plug(prompts=user_input.UserInput)
+@htf.measures(htf.Measurement('scanned_serial').matches_regex(r'\d{4}'))
+def scan_serial(test, prompts):
+    """Scan in a new serial for the DUT."""
+    test.measurements.scanned_serial = prompts.prompt(
+        'Enter a serial (four digits):', text_input=True)
+    if is_fail(test, 'scanned_serial'):
+        test.logger.warning('Invalid serial. Try again.')
+        return htf.PhaseResult.REPEAT
+
+
+@htf.measures(htf.Measurement('original_serial').matches_regex(r'\d{4}'))
+def get_earlier_measurement(test):
+    test.measurements.original_serial = get_measurement(
+        test, 'scan_serial', 'scanned_serial')
 
 
 @htf.measures(
@@ -29,7 +46,10 @@ if __name__ == '__main__':
     htf.conf.load(capture_source=True)
 
     test = htf.Test(
+        scan_serial,
+        get_earlier_measurement,
         fail_mid_phase,
+        get_earlier_measurement,
         test_name='example test'
     )
 
