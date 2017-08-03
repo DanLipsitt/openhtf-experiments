ea0971b1eddb0a10d2faa12750f1a42b2d57f62b
Dan Lipsitt <danlipsitt@gmail.com>
2017-08-03 15:59:44 -0700

    Use a validator and custom `test_start` phase for dut_serial.py.

--- dut_serial.py
+++ dut_serial.py
@@ -1,18 +1,32 @@
 from __future__ import print_function
+
 import openhtf as htf
-from openhtf.plugs.user_input import prompt_for_test_start
-from openhtf.output.callbacks import json_factory
+from openhtf.output.callbacks import console_summary, json_factory
+from openhtf.plugs import user_input
+from openhtf.util.validators import matches_regex, register
+
+
+@register
+def valid_serial():
+    return matches_regex(r'x')
+
+
+@htf.plug(prompts=user_input.UserInput)
+@htf.measures(htf.Measurement('serial').valid_serial())
+def get_serial(test, prompts):
+    serial = prompts.prompt(message="Enter a serial number", text_input=True)
+    test.measurements.serial = serial
+    test.test_record.dut_id = serial
+
 
 if __name__ == '__main__':
-    import sys
+    import os.path
 
-    test = htf.Test(
-        test_name='dut input'
-    )
+    test = htf.Test(test_name='dut input')
 
-    test.add_output_callbacks(json_factory.OutputToJSON(sys.stdout, indent=4))
+    name = os.path.splitext(os.path.basename(__file__))[0]
+    test.add_output_callbacks(
+        json_factory.OutputToJSON(name + '.json', indent=4),
+        console_summary.ConsoleSummary())
 
-    test.execute(
-        test_start=prompt_for_test_start(
-            message="This prompt only accepts 'x' as an answer.",
-            validator=lambda s: s if s == 'x' else False))
+    test.execute(test_start=get_serial)
