7026d46d2686b39edc33631f814fb8bfbcae96b8
Dan Lipsitt <danlipsitt@gmail.com>
2017-08-08 10:20:03 -0700

    Repeat serial input if validation fails.

--- dut_serial.py
+++ dut_serial.py
@@ -4,6 +4,7 @@ import openhtf as htf
 from openhtf.output.callbacks import console_summary, json_factory
 from openhtf.plugs import user_input
 from openhtf.util.validators import matches_regex, register
+from util import is_fail
 
 
 @register
@@ -11,18 +12,32 @@ def valid_serial():
     return matches_regex(r'x')
 
 
+@htf.PhaseOptions(repeat_limit=5)
 @htf.plug(prompts=user_input.UserInput)
 @htf.measures(htf.Measurement('serial').valid_serial())
 def get_serial(test, prompts):
-    serial = prompts.prompt(message="Enter a serial number", text_input=True)
+    serial = prompts.prompt(
+        message="Enter a serial number (must be 'x')", text_input=True)
     test.measurements.serial = serial
+    if is_fail(test, 'serial'):
+        return htf.PhaseResult.REPEAT
     test.test_record.dut_id = serial
+
+
+@htf.plug(prompts=user_input.UserInput)
+@htf.measures(htf.Measurement('value').matches_regex('a'))
+def get_value(test, prompts):
+    test.measurements.value = prompts.prompt(
+        message="Enter the letter 'a'", text_input=True)
 
 
 if __name__ == '__main__':
     import os.path
 
-    test = htf.Test(test_name='dut input')
+    test = htf.Test(
+        get_value,
+        test_name='dut input'
+    )
 
     name = os.path.splitext(os.path.basename(__file__))[0]
     test.add_output_callbacks(
